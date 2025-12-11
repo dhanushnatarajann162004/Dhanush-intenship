69ad2d73b4ef04d033e17ee292e5a000117cc945
dhanushnatarajann162004 <dhanushnatarajan.n@gmail.com>
2025-12-12 00:32:58 +0530

    Add files via upload

---
+++ DAY 1 CODE.py
@@ -0,0 +1,230 @@
+#use only jupiter notebook
+
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+a=5
+a=10
+
+
+# In[4]:
+
+
+a
+
+
+# In[5]:
+
+
+IMPORT KEYWORD
+KEYWORD.KWLIST
+
+
+# In[7]:
+
+
+import keyword
+keyword.kwlist
+
+
+# In[8]:
+
+
+if=65
+if
+
+
+# In[9]:
+
+
+a=65
+a
+
+
+# In[10]:
+
+
+type(a)
+
+
+# In[11]:
+
+
+id(a)
+
+
+# In[12]:
+
+
+0B1010
+
+
+# In[13]:
+
+
+0O1010
+
+
+# In[14]:
+
+
+0X1010
+
+
+# In[15]:
+
+
+0X1010A
+
+
+# In[16]:
+
+
+3e7
+
+
+# In[18]:
+
+
+3e1
+
+
+# In[19]:
+
+
+5e7
+
+
+# In[20]:
+
+
+hex(21)
+
+
+# In[21]:
+
+
+oct(21)
+
+
+# In[22]:
+
+
+bin(21)
+
+
+# In[23]:
+
+
+'HI'
+
+
+# In[24]:
+
+
+"GOOD"
+
+
+# In[25]:
+
+
+"""
+HI
+GOOD
+MORNING
+"""
+
+
+# In[26]:
+
+
+true
+
+
+# In[27]:
+
+
+True
+
+
+# In[28]:
+
+
+type(True)
+
+
+# In[29]:
+
+
+False
+
+
+# In[30]:
+
+
+type(False)
+
+
+# In[31]:
+
+
+A=23.336j 
+print(A)
+
+
+# In[32]:
+
+
+A=23.336j 
+print(A.real)
+
+
+# In[33]:
+
+
+A=23.336j 
+print(A.imag)
+
+
+# In[34]:
+
+
+id(A)
+
+
+# In[35]:
+
+
+A=4
+id(A)
+
+
+# In[36]:
+
+
+d=4
+id(d)
+
+
+# In[37]:
+
+
+D=257
+id(D)
+
+
+# In[38]:
+
+
+A=257
+id(A)
+
+
+# In[ ]:
+
+
+
+
